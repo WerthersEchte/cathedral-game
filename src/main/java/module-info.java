16aaccb026f8af03f22b4cdcf18e276a02e536ac
Eike Petersen <eike.m.petersen@gmail.com>
2022-03-28 22:59:28 +0200

    added Java-doc to Turn and module-info
    Code-style fixes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,9 @@
+/**
+ * A module containing all the necessary logic to play the game Cathedral.
+ *
+ * @author Eike Petersen {@literal <eike.petersen@fh-kiel.de>}
+ * @since 1.0 Beta
+ */
 module cathedral.game {
   exports de.fhkiel.ki.cathedral;
 }
